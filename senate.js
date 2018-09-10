46c7ebdd97d86fd984ce71e517aab0a03034667f
Vanya Delova <happy.vanya.delova@gmail.com>
2018-09-10 11:21:50 +0200

    Bck to Top Button

--- senate.js
+++ senate.js
@@ -145,6 +145,19 @@ document.getElementById("stateSelect").addEventListener("change", function () {
 
 
 
+$(document).ready(function(){ 
+//    $(window).scroll(function(){ 
+//        if ($(this).scrollTop() > 100) { 
+//            $('#scroll').fadeIn(1000); 
+//        } else { 
+//            $('#scroll').fadeOut(1000); 
+//        } 
+//    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
 
 
 
--- statistics.js
+++ statistics.js
@@ -325,3 +325,17 @@ function mostTenPctLoy() {
 //}
 
 }
+
+$(document).ready(function(){ 
+//    $(window).scroll(function(){ 
+//        if ($(this).scrollTop() > 100) { 
+//            $('#scroll').fadeIn(1000); 
+//        } else { 
+//            $('#scroll').fadeOut(1000); 
+//        } 
+//    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
